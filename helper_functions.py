2a132d83d64cd17824c5fa937513f28193b0fb1c
UnnarThorBachmann <uthb@hotmail.com>
2016-08-12 00:32:24 +0000

    style and refactor: Adding the comments to the project_item_catalog.py and refactoring

--- helper_functions.py
+++ helper_functions.py
@@ -32,7 +32,7 @@ def make_pw_hash(name, pw,salt=None):
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s|%s' % (h, salt)
 
-def valid_pw(name, pw, h):
+def valid_password(name, pw, h):
     a = h.split('|')
     salt = a[1]
     val = a[0]
--- project_item_catalog.py
+++ project_item_catalog.py
@@ -1,3 +1,12 @@
+"""
+Author: Unnar Thor Bachmann.
+"""
+
+"""
+This module uses flask and sqlalchemy to make a multi user CRUD page.
+Each user can create and update his item if logged in. Each item is in a different category. 
+"""
+
 from flask import Flask,request, render_template, redirect
 from flask import url_for, make_response, session as login_session
 from flask import flash, jsonify
@@ -7,12 +16,16 @@ from database_setup import Category, Base, Item, User
 import json
 import random
 import string
-import helper_functions
 
+# These helper functions were created in the multi user blog project.
+from helper_functions import valid_username, valid_password, valid_email, make_salt, make_pw_hash
+
+#Creating the app flask.
 app = Flask(__name__)
-app.secret_key = 'unnar'
+app.secret_key = '94A4QZCD4Q91YWGQ6PTH12YHTBELYR4A'
 
 
+#Connecting with a database.
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
@@ -22,7 +35,12 @@ session = DBSession()
 @app.route('/')
 @app.route('/catalog')
 def showCatalog():
+    """
+    This method renders the main page of the app. 
+    """
     filterCategoryName = 'none'
+
+    # Rendering the page differently when user is logged in.
     if login_session.has_key('username'):
        addButtonHide = ''
        logoutButtonHide = ''
@@ -46,15 +64,21 @@ def showCatalog():
 
 @app.route('/catalog/<categoryName>/items')
 def showCategory(categoryName):
+    """
+    This page render the main page of the app were only items
+    in give category are rendered.
+    """
     category = session.query(Category).filter_by(name=categoryName).first()
     filterCategory=categoryName
 
+    # Rendering the page differently when user is logged in.
     if login_session.has_key('username'):
        addButtonHide = ''
        logoutButtonHide = ''
        loginButtonHide = 'hidden'
        category = session.query(Category).filter_by(name=categoryName).first()
-       items = session.query(Item).filter(Item.user_id==login_session['id']).filter(Item.category==category).all()
+       query = session.query(Item).filter(Item.user_id==login_session['id'])
+       items = query.filter(Item.category==category).all()
     else:
         addButtonHide = 'hidden'
         logoutButtonHide = 'hidden'
@@ -73,6 +97,11 @@ def showCategory(categoryName):
 
 @app.route('/catalog/<categoryName>/<itemName>')
 def showItem(itemName,categoryName):
+    """
+    Renders a page for a given item.
+    """
+    
+    # Rendering the page differently when user is logged in.
     if login_session.has_key('username'):
        logoutButtonHide = ''
        loginButtonHide = 'hidden'
@@ -84,6 +113,9 @@ def showItem(itemName,categoryName):
     
     filterItem = session.query(Item).filter_by(name=itemName).first()
     email = filterItem.user.email
+
+    # Renders the page only if no user is logged in or if current user
+    # has an id matching the user_id of the item
     if filterItem is not None and filterItem.category.name == categoryName\
        and (not login_session.has_key('id') or login_session['id'] == filterItem.user_id):
        return render_template('item.html',
@@ -93,7 +125,6 @@ def showItem(itemName,categoryName):
                               editDeleteHide=editDeleteHide,
                               email=email)
     else:
-        # Not sure if this is the right response.
         response = make_response(json.dumps("File not found"), 404)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -101,74 +132,109 @@ def showItem(itemName,categoryName):
 
 @app.route('/catalog/<itemName>/edit', methods = ['POST','GET'])
 def editItem(itemName):
-    if login_session.has_key('username'):
-       logoutButtonHide = ''
-       loginButtonHide = 'hidden'
-    else:
-        return redirect(url_for('showLogIn',
-                                signup='false'))
+    """
+    This function is for editing an item.
 
-    if request.method == 'GET':
-       
-       categories = session.query(Category).all()
-       filterItem = session.query(Item).filter_by(name=itemName).first()
+    GET: Renders a form for editing the name, description and category.
     
-       if filterItem is not None and filterItem.user_id == login_session['id']:          
-          categories = session.query(Category)
-          login_session['itemId'] = int(filterItem.id)
-          return render_template('edit.html',
-                             item=filterItem,
-                             categories=categories,
-                             logoutButtonHide=logoutButtonHide,
-                             loginButtonHide=loginButtonHide)
+    POST: Updates the item in the database and redirects to the main page.
+    """
+
+    if request.method == 'GET':  
+       # The page is only rendered if there is a user logged in.
+       if login_session.has_key('username'):
+          logoutButtonHide = ''
+          loginButtonHide = 'hidden'
+          categories = session.query(Category).all()
+          # Splitting the query do to length. The item is filtered
+          # with respect to name and the id of the current user.
+          query =  session.query(Item).filter_by(name=itemName)
+          filterItem = query.filter_by(user_id = int(login_session['id'])).first()
+
+          #If the item does not exist the page is not rendered.
+          if filterItem is not None:          
+             categories = session.query(Category)
+             #The item id is put into the loggin session for editing.
+             login_session['itemId'] = int(filterItem.id)
+             return render_template('edit.html',
+                                    item=filterItem,
+                                    categories=categories,
+                                    logoutButtonHide=logoutButtonHide,
+                                    loginButtonHide=loginButtonHide)
+          else:
+              response = make_response(json.dumps("File not found"), 404)
+              response.headers['Content-Type'] = 'application/json'
+              return response
        else:
-           response = make_response(json.dumps("File not found"), 404)
-           response.headers['Content-Type'] = 'application/json'
-           return response
+           return redirect(url_for('showLogIn',
+                           signup='false'))
+       
     else:
+        #POST
+        #The form is read.
         itemName = request.form['name']
         itemDescription = request.form['description']   
         itemCategory = session.query(Category).filter_by(name=request.form['category']).first()
+        #Item has to be selected by id. The name could have changed.
         itemEdited = session.query(Item).filter_by(id=login_session['itemId']).first()
         
-        
+        # Returns error if the item does not exist or if current user is not
+        # the owner of it.
         if itemEdited is None or itemEdited.user_id != login_session['id']:
            response = make_response(json.dumps("File not found"), 404)
            response.headers['Content-Type'] = 'application/json'
            return response
-        
+
+        # Item is updated
         itemEdited.name=itemName
         itemEdited.description = itemDescription
         itemEdited.category = itemCategory
         itemEdited.category_id = itemCategory.id
         itemEdited.user = session.query(User).filter_by(name=login_session['username']).first()
         itemEdited.user_id = int(login_session['id'])
+        
+        # The database is updated.
         session.add(itemEdited)
         session.commit()
         return redirect(url_for('showCatalog'))
         
 @app.route('/catalog/new', methods = ['POST','GET'])
 def newItem():
-    if login_session.has_key('username'):
-       logoutButtonHide = ''
-       loginButtonHide = 'hidden'
+    """
+    This function is for createing a new item.
+
+    GET: Renders the form for making a new item.
+    POST: Creates a new item.
+    """
+    if request.method == 'GET':
+       # If user is not logged in the he is redirected
+       # to the log in page.
+       if login_session.has_key('username'):
+          logoutButtonHide = ''
+          loginButtonHide = 'hidden'
+          categories = session.query(Category).all()
+          return render_template('newItem.html',
+                                 categories = categories,
+                                 logoutButtonHide=logoutButtonHide,
+                                 loginButtonHide=loginButtonHide)
+       else:
+           redirect(url_for('showLogIn',
+                            signup='false'))
+            
+       
     else:
-        redirect(url_for('showLogIn',
-                         signup='false'))
-    
-    if request.method == 'GET':   
-       categories = session.query(Category).all()
-       return render_template('newItem.html',
-                              categories = categories,
-                              logoutButtonHide=logoutButtonHide,
-                              loginButtonHide=loginButtonHide)
-    else:   
+        #POST
+        
+        #Reading the form.
         itemName = request.form['name']
         itemDescription = request.form['description']
         itemCategory = request.form['category']
-        
+
+        # Finding the user and the category of the item from database.
         user = session.query(User).filter_by(name=login_session['username']).first()
         category = session.query(Category).filter_by(name=itemCategory).first()
+
+        #Creating the user
         newItem = Item(name=itemName,
                        description = itemDescription,
                        user_id = int(login_session['id']),
@@ -231,10 +297,8 @@ def loginUser():
     password  = request.form['logInPassword']
     
           
-    valid_usr = helper_functions.valid_username(username)
-    valid_pw = helper_functions.valid_username(password)
-    #If sentence to detect error in login.
-    error_login = ''
+    valid_usr = valid_username(username)
+    valid_pw = valid_username(password)
   
     if (valid_usr is None or valid_pw is None):
         flash(username,'login')
@@ -244,10 +308,9 @@ def loginUser():
                                 signup='false'))
     else:
         user = session.query(User).filter_by(name=username).first()
-        
-        if user is None or user.password is None or not helper_functions.valid_pw(username,
-                                                                                  password,
-                                                                                  user.password):
+        if user is None or user.password is None or not valid_password(username,
+                                                                 password,
+                                                                 user.password):
            flash(username,'login')
            flash('Invalid login','login')
            return redirect(url_for('showLogIn',
@@ -275,9 +338,9 @@ def signUp():
     
     
     flash(username,'signup')
-    valid_usr = helper_functions.valid_username(username)
-    valid_pw = helper_functions.valid_username(password)
-    valid_em = helper_functions.valid_email(email)
+    valid_usr = valid_username(username)
+    valid_pw = valid_username(password)
+    valid_em = valid_email(email)
 
     if (valid_usr is None):
         flash('Invalid username','signup')
@@ -287,7 +350,7 @@ def signUp():
         else:
             flash('','signup')
             
-    if (valid_pw is None):
+    if (valid_psw is None):
        flash("That wasn't a valid password",'signup')
        flash('','signup')
     else:
@@ -310,10 +373,10 @@ def signUp():
                                logoutButtonHide='hidden',
                                signup='true')
     else:
-        salt = helper_functions.make_salt()
+        salt = make_salt()
         newUser = User(name=username,
                        email = email,
-                       password=helper_functions.make_pw_hash(username,password,salt))
+                       password=make_pw_hash(username,password,salt))
         session.add(newUser)
         session.commit()
         login_session['username'] = username
